4fad7d464de9d69846fc48f760140449cf0931fd
dalifyyy <d4lified@gmail.com>
2026-02-11 18:12:47 +0800

    .

--- scenes/objects/leg/leg.gd
+++ scenes/objects/leg/leg.gd
@@ -1,11 +1,7 @@
 extends Node2D
 
+@export var key:String = ""
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(_delta):
+	if Global.input(key):
+		rotate(rotation_degrees + 0.1)
